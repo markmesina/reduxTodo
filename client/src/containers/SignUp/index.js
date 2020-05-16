f24a3a9982998a17a041ec6020293c586fac70d4
ceress <markmesina69@gmail.com>
2020-05-16 14:18:13 -0700

    created redux form

--- client/src/containers/SignUp/index.js
+++ client/src/containers/SignUp/index.js
@@ -6,20 +6,39 @@ import { email, length, required } from 'redux-form-validators';
 class SignUp extends Component {
 
 
-  renderEmail = formProps => {
-    console.log(formProps, 'this is form pros');
+  renderEmail = ({ input, meta }) => {
+    console.log(meta)
     return (
       <Form.Input
+        {...input}
+        error = { meta.touched && meta.error }
         fluid
         icon = 'user'
         iconPosition = 'left'
         autoComplete = 'off'
         placeholder = 'Email Address'
       />
-    )
+    );
   }
 
+  renderPassword = ({ input, meta }) => {
+    return (
+      <Form.Input
+        {...input}
+        error = { meta.touched && meta.error }
+        fluid
+        type='password'
+        icon='lock'
+        iconPosition = 'left'
+        autoComplete = 'off'
+        placeholder = 'Password'
+      />
+    );
+  }
+
+
   render() {
+    console.log('inside of signup render', this.props);
     return (
       <Form size='large' >
         <Segment stacked>
@@ -33,11 +52,21 @@ class SignUp extends Component {
             ]
           }
           />
+          <Field
+            name = 'password'
+            component = {this.renderPassword}
+            validate = {
+              [
+                required({msg: 'You must provide a password'}),
+                length({ min: 6, msg: 'Password must be at least 6 characters' }),
+              ]
+            }
+          />
 
         </Segment>
       </Form>
     );
   }
-}
+};
 //HOC export
 export default reduxForm({ form: 'signup' })(SignUp);
